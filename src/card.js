6d3f3257fcf3c01a79e13ea0f7609da2fa6b9e30
James Knippel <jamesknippel@gmail.com>
2020-10-12 23:29:51 -0700

    tachyon != tachyons

--- src/card.js
+++ src/card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
   // Can only return one element per react component, unless it's an array presumably
   return (
-    <div>
+    <div className="f1 tc">
       <img alt="robot pic" src="https://robohash.org/test?200x200"></img>
       <h2>Test Name</h2>
       <p>Blah blah biography</p>
