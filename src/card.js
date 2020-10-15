7f49175980a3ade1a13aff96121bffbf80958ea9
James Knippel <jamesknippel@gmail.com>
2020-10-14 23:15:56 -0700

    green get

--- src/card.js
+++ src/card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
   // Can only return one element per react component, unless it's an array presumably
   return (
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="robot pic" src={`"https://robohash.org/${props.id}?200x200"`}></img>
+      <img alt="robot avatar" src={`"https://robohash.org/${props.id}?200x200"`}></img>
       <h2>{props.name} : {props.id}</h2>
       <p>{props.email}</p>
       <p>I weight {props.weight} pounds!</p>
