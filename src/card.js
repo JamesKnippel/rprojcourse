d37c1a6494471540e9690f3a129be6d75cebbb72
James Knippel <jamesknippel@gmail.com>
2020-10-15 22:38:18 -0700

    destructured + prop

--- src/card.js
+++ src/card.js
@@ -4,13 +4,13 @@ import React from 'react';
 
 
 
-const Card = (props) => {
+const Card = ({ name, email, id}, props) => {
   // Can only return one element per react component, unless it's an array presumably
-  const { name, email, id } = props;
+  // const { name, email, id } = props;
   return (
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <img alt="robot avatar" src={`"https://robohash.org/${id}?200x200"`}></img>
-      <h2>{name} : {props.id}</h2>
+      <h2>{name} : {id}</h2>
       <p>{email}</p>
       <p>I weight {props.weight} pounds!</p>
     </div>
