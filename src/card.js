d5c74e08a36697a6ead6b9d216a9456035103d3a
James Knippel <jamesknippel@gmail.com>
2020-10-15 22:37:41 -0700

    const props style

--- src/card.js
+++ src/card.js
@@ -6,11 +6,12 @@ import React from 'react';
 
 const Card = (props) => {
   // Can only return one element per react component, unless it's an array presumably
+  const { name, email, id } = props;
   return (
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="robot avatar" src={`"https://robohash.org/${props.id}?200x200"`}></img>
-      <h2>{props.name} : {props.id}</h2>
-      <p>{props.email}</p>
+      <img alt="robot avatar" src={`"https://robohash.org/${id}?200x200"`}></img>
+      <h2>{name} : {props.id}</h2>
+      <p>{email}</p>
       <p>I weight {props.weight} pounds!</p>
     </div>
   );
