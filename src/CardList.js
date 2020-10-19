aaaccd03f89c5123ea371a3df3ecc4659ff7d5a5
James Knippel <jamesknippel@gmail.com>
2020-10-19 16:20:23 -0700

    sans variable

--- src/CardList.js
+++ src/CardList.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import Card from './card'
+import React, { Component } from "react";
+import Card from "./card";
 
 const CardList = ({ robots }) => {
-  const cardIterator = robots.map((robot, i) => {
-    return <Card key={i} weight={Math.random()} id={robots[i].id} name={robots[i].name} email={robots[i].email} />
-  })
-    return (
+  return (
     <div>
-      {/* <Card weight={Math.random()} id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card weight={Math.random()} id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card weight={Math.random()} id={robots[2].id} name={robots[2].name} email={robots[2].email}/> */}
-      {cardIterator}
-      <Card weight={Math.random()}/>
-      <Card weight={Math.random()}/>
+      {robots.map((robot, i) => {
+        return (
+          <Card
+            key={i}
+            weight={Math.random()}
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+          />
+        );
+      })}
     </div>
-    );
-  
-}
+  );
+};
 
 export default CardList;
