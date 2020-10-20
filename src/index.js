ef6948abd7d655a8b3d54d367ab174d38fe1bcb1
James Knippel <jamesknippel@gmail.com>
2020-10-20 13:35:04 -0700

    angular still rocks

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 
-import CardList from './CardList';
-import App from './App';
+import App from './containers/App';
 
 import 'tachyons';
 
-import { robots } from './robots';
 
 import * as serviceWorker from "./serviceWorker";
 
