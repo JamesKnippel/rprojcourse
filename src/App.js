8ac58594e5c51bca800532ed5e30a3b7a4752d59
James Knippel <jamesknippel@gmail.com>
2020-10-20 12:56:46 -0700

    swapped around some this context

--- src/App.js
+++ src/App.js
@@ -14,16 +14,20 @@ class App extends React.Component {
     };
   }
 
-  onSearchChange(event) {
-    console.log(event.target.value);
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
   }
 
   render() {
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    console.log(filteredRobots);
     return (
       <div className="tc">
         <h1> RoboFriends </h1>
         <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={this.state.robots} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
