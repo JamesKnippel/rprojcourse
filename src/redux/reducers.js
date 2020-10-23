cd2402c48d5f3c3d8bb9224604250ce3255a6a2d
James Knippel <jamesknippel@gmail.com>
2020-10-22 22:12:17 -0700

    redux scaffolded

---
+++ src/redux/reducers.js
@@ -0,0 +1,16 @@
+import { CHANGE_SEARCH_FIELD } from 'constants';
+
+const initialState = {
+  searchField: '',
+}
+
+export const searchRobots = (state=initialState, action = {}) => {
+  switch (action.type) {
+    case CHANGE_SEARCH_FIELD:
+      // return Object.assign({}, state, searchField: action.payload) // traditional style
+      return { ...state, searchField: action.payload } // es6/es7
+    
+    default:
+      return state;
+  }
+}
